04706eae3d79d4cfc5ce34b422f907b59dc5b2d2
raul zambrano <raulzambranoleon15@gmail.com>
2026-04-24 01:42:16 -0500

    feat: modelo rol

---
+++ src/main/java/com/dolusa/backend/model/Rol.java
@@ -0,0 +1,20 @@
+package com.dolusa.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+}
