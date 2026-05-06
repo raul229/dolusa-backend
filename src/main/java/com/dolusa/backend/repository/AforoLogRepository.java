6f1642bd165a000820562eb15ee630e5d53e1e26
raul zambrano <raulzambranoleon15@gmail.com>
2026-05-06 01:09:39 -0500

    fix: borre modelos no contemplados

--- src/main/java/com/dolusa/backend/repository/AforoLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dolusa.backend.repository;
-
-import com.dolusa.backend.model.AforoLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AforoLogRepository extends JpaRepository<AforoLog, Long> {}
--- src/main/java/com/dolusa/backend/repository/ZonaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dolusa.backend.repository;
-
-import com.dolusa.backend.model.Zona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZonaRepository extends JpaRepository<Zona, Long> {}
